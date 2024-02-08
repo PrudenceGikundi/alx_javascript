5ad25937df99bc066de31ab1c26c9f99572e6dbc
PrudenceGikundi <prudencekendi101@gmail.com>
2024-02-08 07:24:06 +0300

    starwars

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,14 @@
+// Script that prints the title po starwars movies episodes
+
+const process = require('process')
+const request = require('request')
+const endpoint = 'https://swapi-api.alx-tools.com/api/films'
+const episode = process.argv[2]
+
+request(`${endpoint}/${episode}`, function(error, response, body){
+    if(error){
+        console.log(error)
+    }
+    const result = JSON.parse(body)
+    console.log(result['title'])
+});
